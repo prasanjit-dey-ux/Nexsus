beb4ad4367876e633bfde3b04d75ef0ada7baa48
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-09-17 15:29:48 +0530

    added express server

---
+++ apps/http/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import cors from 'cors';
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("HTTP Server is running!");
+});
+
+// Start the server
+app.listen(port, ()=> {
+    console.log(`HTTP server listening on http://localhost:${port}`);
+});
