cde69577ff9c107cd9f188fd98e566162446760f
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-09-26 03:50:29 +0530

    added user routes

---
+++ apps/http/src/routes/v1/userRouter.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+export const userRouter = Router();
+
+userRouter.get("/me", (req, res) => {
+    console.log("get the profile");
+});
+
+userRouter.put("/me", (req, res) => {
+    console.log("Update user profile");
+})
+
+userRouter.put("me/password", (req, res) => {
+    console.log("Change password");
+});
+
