0ebb184c499b4177c3ead956f050e35ca89ac299
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-09-26 17:10:12 +0530

    updated message and notification router

--- apps/http/src/routes/v1/messageRouter.ts
+++ apps/http/src/routes/v1/messageRouter.ts
@@ -1,3 +1,12 @@
 import { Router } from "express";
 
 export const messageRouter = Router();
+
+messageRouter.get("/", (req, res) => {
+    console.log("Get all the messages");
+});
+
+messageRouter.get("/:id", (req, res) => {
+    console.log("Get specific messages");
+});
+
--- apps/http/src/routes/v1/notificationRouter.ts
+++ apps/http/src/routes/v1/notificationRouter.ts
@@ -1,3 +1,15 @@
 import { Router } from "express";
 
 export const notificationRouter = Router();
+
+notificationRouter.get("/", (req, res) => {
+    console.log("Get all the notification");
+});
+
+notificationRouter.patch("/:id/read", (req, res) => {
+    console.log("Notfication marked as Read");
+});
+
+notificationRouter.delete("/:id", (req, res) => {
+    console.log("Remove the notification");
+})
