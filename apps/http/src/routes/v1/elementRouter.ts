ff929d9cb7d46941aa4d1cb762b3ede0c76999ff
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-09-26 17:09:54 +0530

    updated map and element router

--- apps/http/src/routes/v1/elementRouter.ts
+++ apps/http/src/routes/v1/elementRouter.ts
@@ -1,3 +1,20 @@
 import { Router } from "express";
 
 export const elementRouter = Router();
+
+elementRouter.get("/", (req, res) => {
+    console.log("element list");
+});
+
+elementRouter.get("/:id", (req, res) => {
+    console.log("element details")
+});
+
+// Admin permission
+elementRouter.post("/", (req, res) => {
+    console.log("Add elements")
+});
+
+elementRouter.delete("/:id", (req, res) => {
+    console.log("Delete elements")
+})
--- apps/http/src/routes/v1/mapRouter.ts
+++ apps/http/src/routes/v1/mapRouter.ts
@@ -8,4 +8,16 @@ mapRouter.get("/", (req, res) => {
 
 mapRouter.get("/:id", (req, res)=> {
     console.log("get the one map")
-})
+});
+
+mapRouter.post("/", (req, res) => {}) //Admin Creat Map 
+mapRouter.put("/:id", (req, res) => {}) //Admin Update Map
+mapRouter.delete("/:id", (req, res) => {}) //Admin Delete Map
+
+// Element management (admin/premium);
+
+mapRouter.get("/:id/elements", (req, res) => {}) // Get the elements from the map
+mapRouter.post("/:id/elements", (req, res) => {}) // Add Element in the map
+mapRouter.delete("/:id/elements/:elementId", (req, res) => {}) // Delete element from the map
+
+
