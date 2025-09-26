e80a50712547083a4aab844a6f71e287e57d074b
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-09-26 17:09:32 +0530

    updated avatar routers

--- apps/http/src/routes/v1/avatarRouter.ts
+++ apps/http/src/routes/v1/avatarRouter.ts
@@ -1,3 +1,15 @@
 import { Router } from "express";
 
 export const avatarRouter = Router();
+
+avatarRouter.get("/", (req, res) => {
+    console.log("Get all the avatar")
+});
+
+avatarRouter.get("/:id", (req, res) => {
+    console.log("Avatar details")
+})
+
+// Admin
+avatarRouter.post("/", (req, res) => {}) // Add avatar
+avatarRouter.delete("/:id", (req, res) => {}) // Delete avatar
