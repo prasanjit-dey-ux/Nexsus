995763eaf121d87d9e1a9ca526deceff64cefc37
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-09-26 17:09:42 +0530

    added utility router

--- apps/http/src/routes/v1/utilityRouter.ts
+++ apps/http/src/routes/v1/utilityRouter.ts
@@ -1,3 +1,11 @@
 import { Router } from "express";
 
 export const utilityRouter = Router();
+
+utilityRouter.get("/health", (req, res) => {
+    console.log("Check health");
+});
+
+utilityRouter.get("/ping", (req, res) => {
+    console.log("Check ping");
+})
