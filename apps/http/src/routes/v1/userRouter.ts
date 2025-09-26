8c0698059ff69ea86dbd7f80a19bd142a9b52275
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-09-26 17:10:23 +0530

    updated user router

--- apps/http/src/routes/v1/userRouter.ts
+++ apps/http/src/routes/v1/userRouter.ts
@@ -14,3 +14,14 @@ userRouter.put("/me/password", (req, res) => {
     console.log("Change password");
 });
 
+userRouter.get("/:id", (req, res) => {
+    console.log("Fetch another profile")
+});
+
+userRouter.get("/me/avatars", (req, res) => {
+    console.log("Get all my avatars")
+})
+
+userRouter.get("/me/messages", (req, res) => {}); //list my DM
+userRouter.get("/me/messages/:userId", (req, res) => {}); // DM conversation with user
+userRouter.post("/me/messages/:userId", (req, res) => {}); // Send DM
