1918ddb3ff92a79cbfdabe883282e62a4e3aee58
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-09-30 20:45:53 +0530

    added jwt token

--- packages/utils/src/jwt.ts
+++ packages/utils/src/jwt.ts
@@ -1,6 +1,22 @@
 import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET!;
+
 export interface JwtPayload {
     userId: string;
-    iat: number;
-    exp: number;
+    iat: number; // issued at
+    exp: number; // expired at
+}
+
+export const generateJWT = (userId: string) : string => {
+    return jwt.sign({ userId }, process.env.JWT_SECRET!, {expiresIn: "7d"})
+};
+
+export const verifyJWT = (token: string) : JwtPayload => {
+    try {
+        return jwt.verify(token, JWT_SECRET) as JwtPayload;
+    } catch (error) {
+        throw new Error ("Invalid or Expired token")
+    }
+    
 }
