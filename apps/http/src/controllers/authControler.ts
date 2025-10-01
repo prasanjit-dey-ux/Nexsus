cf116b86167819b5758730ff719c3127055c1345
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-10-01 18:27:54 +0530

    add google auth controller

--- apps/http/src/controllers/authControler.ts
+++ apps/http/src/controllers/authControler.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from "express";
 import { googleAuthService } from "../service/authService";
-import { error } from "console";
-
 
 export const googleAuth = async (req: Request, res: Response): Promise<void> => {
     try {
@@ -13,6 +11,7 @@ export const googleAuth = async (req: Request, res: Response): Promise<void> => 
         const result = await googleAuthService(idToken);
         res.json(result);
     } catch (error: any) {
-        res.status(500).json({ error: error.message || "OAuth failed"});
+        const errorMessage = error instanceof Error ? error.message : "OAuth failed"
+        res.status(500).json({ error: errorMessage});
     }
 }
