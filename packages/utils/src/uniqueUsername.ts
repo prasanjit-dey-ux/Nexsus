2ac3616dcc65fdaeaa3ffe549bb4de23c72f0e0d
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-10-03 23:01:35 +0530

    update username generation

--- packages/utils/src/uniqueUsername.ts
+++ packages/utils/src/uniqueUsername.ts
@@ -1,13 +1,49 @@
-export async function generateUniqueUsername(desired: string, client: any) {
-    let finalUsername = desired.replace(/[^a-zA-Z0-9_]/g,"");
+export async function generateUniqueUsername(desired: string,client:any) {
+    let finalUsername = desired.replace(/^a-zA-Z0-9_]/g,"");
     let attempt = 0;
 
-    while (await client.user.findUnique({where: {username: finalUsername }})) {
+    while (await client.user.findUnique({ where: { username: finalUsername } })) {
         finalUsername = `${desired}${Math.floor(1000 + Math.random() * 9000)}`;
         attempt ++;
 
-        if (attempt >= 10 ) throw new Error ("Could not generate unique username");
+        if(attempt >= 10) throw new Error("Could not generate unique username");       
     }
+    return finalUsername;        
+}
 
-    return finalUsername;
+export async function createUser({
+    email,
+    baseUsername,
+    provider,
+    providerId,
+    displayName,
+    profileImageUrl,
+    bannerImageUrl = "",
+    bio = "",
+    client
+}: {
+    email: string;
+    baseUsername: string;
+    provider: string;
+    providerId: string;
+    displayName?: string;
+    profileImageUrl?: string;
+    bannerImageUrl?: string;
+    bio?: string;
+    client: any;
+}) {
+    const finalUsername = await generateUniqueUsername(baseUsername, client);
+    return await client.user.create({
+        data: {
+            email,
+            provider,
+            providerId,
+            username: finalUsername,
+            displayName: displayName || finalUsername,
+            profileImageUrl: profileImageUrl || "",
+            bannerImageUrl: bannerImageUrl || "",
+            isProfileCompleted: false,
+            bio 
+        },
+    });
 }
